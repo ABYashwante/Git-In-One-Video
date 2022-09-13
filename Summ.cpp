f73568e98ed4b5b932d38da23ae99b8f79a9cab1
Avishkar <aviyashwante1911@gmail.com>
2022-09-13 18:57:47 +0530

    Initial commit

---
+++ Summ.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a = 2;
+    int b = 5;
+    cout<<a+b<<endl;
+    return 0;
+}
